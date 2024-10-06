b4a244eee8ba74c3a849deefaa82b6b3f9112ba7
Farrux20030308 <kurieito0001@gmail.com>
2024-10-06 19:03:42 +0500

    2nd homework start

---
+++ app.js
@@ -0,0 +1,12 @@
+let x,y,z,res;
+
+x =Number(prompt("Запишите число: "))
+y =Number(prompt("Запишите число: "))
+z =Number(prompt("Запишите число: "))
+res = (x+y+z)/3;
+
+
+
+
+
+console.log("Вы ввели числа: " + x + "," + y + "," + z + ". Среднее число будет:" + res);
